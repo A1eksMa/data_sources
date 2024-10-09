aa556d5e41449df7cd0ba317dea3a242ad5f5b13
A1eksMa <a1ex_ma@mail.ru>
2024-10-09 17:09:59 +0300

    Add test to Timing()

--- data_sources/timing.py
+++ data_sources/timing.py
@@ -2,19 +2,41 @@ from datetime import datetime
 
 class Timing():
     """
+    Вспомогательный класс, для обработки показателей даты/времени.
+    Обеспечивает некоторый полиморфизм входных данных (при инциализации),
+    по умолчанию инициализируется значением текущей даты/времени.
+
+    Объект гарантировано содержит атрибуты даты/времени в формате:
+    dt: datetime
+    ut: unix-time
+    dts: datetime в формате строки
+    uti: unix-time в формате целого числа (в милисикундах)
+    uts: unix-time в формате строкового отображения целого числа (в милисикундах)
+    
     """
 
     def __init__(self, dt = datetime.now()):
         # Date, time in datetime format
+        # from datetime
         if isinstance(dt, datetime):
             self.dt = dt
-        if isinstance(dt, tuple):
+        # from tuple
+        # Tuple should be looks like a (value: str, pattern: str).
+        # For example:
+        # ('2020-01-01 00:00:00', '20'+'%y-%m-%d %H:%M:%S')
+        if isinstance(dt, tuple): 
             self.dt = datetime.strptime(dt[0], dt[1])
+        # from string
+        if isinstance(dt, str):
+            pass
 
         # Date, time in unix-time format
         self.ut = self.dt.timestamp()
-
-
+        # Other formats
+        self.dts = self.dt_string()
+        self.uts = self.ut_string()
+        self.uti = self.ut_integer()
+        
     def dt_string(self, pattern: str = '') -> str:
         if pattern:
             return self.dt.strftime(pattern)
@@ -24,14 +46,5 @@ class Timing():
     def ut_string(self) -> str:
         return str(int(self.ut*1000000))
 
-    @staticmethod
-    def now() -> datetime:
-        return datetime.now()
-
-    def set(self,  value: str, pattern: str):
-        """
-        Looks like a:
-        ('2020-01-01 00:00:00', '20'+'%y-%m-%d %H:%M:%S')
-        """
-        self.dt = datetime.strptime(value, pattern)
-        self.ut = self.dt.timestamp()
+    def ut_integer(self) -> int:
+        return int(self.ut*1000000)
