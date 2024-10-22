2761ae66914f624f70dd8ed8178f7a24a6f542e4
A1eksMa <a1ex_ma@mail.ru>
2024-10-22 13:03:37 +0300

    Remove old files

--- data_sources/main.py
+++ data_sources/main.py
@@ -1,23 +1,24 @@
 import argparse
-import logging
-
 from lib import *
-
+   
 
 def test():
     print('Test mode')
 
+
 def main():
     print('Main mode')
-
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage=__doc__)
+    
     parser.add_argument('-t', '--test',
                         dest='test',
                         action='store_true',
                         default=False,
                         help='Test mode')
+    
     args = parser.parse_args()
 
     test() if args.test else main()
